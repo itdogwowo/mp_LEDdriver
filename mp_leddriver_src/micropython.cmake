ecdd985baa0488375f3c6e16194e93b8674f90b9
TVB-OA\bl91920 <itdogwowo@gmail.com>
2026-03-04 14:47:13 +0800

    no message

---
+++ mp_leddriver_src/micropython.cmake
@@ -0,0 +1,17 @@
+# Create an INTERFACE library for our module
+add_library(mp_leddriver_lib INTERFACE)
+
+# Add source files
+target_sources(mp_leddriver_lib INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/mp_leddriver.c
+    ${CMAKE_CURRENT_LIST_DIR}/bus_i8080.c
+    ${CMAKE_CURRENT_LIST_DIR}/strip_encoder.c
+)
+
+# Add include directories
+target_include_directories(mp_leddriver_lib INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}
+)
+
+# Link to the main MicroPython target
+target_link_libraries(usermod INTERFACE mp_leddriver_lib)
