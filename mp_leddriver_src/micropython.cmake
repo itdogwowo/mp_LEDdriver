c875d2ec649eb626a8440b551af8f5d4c20e7051
TVB-OA\bl91920 <itdogwowo@gmail.com>
2026-03-04 15:00:18 +0800

    no message

--- mp_leddriver_src/micropython.cmake
+++ mp_leddriver_src/micropython.cmake
@@ -13,9 +13,15 @@ target_include_directories(mp_leddriver_lib INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
+# Add dependency on ESP-IDF components
+# This ensures header paths for esp_lcd, driver, and heap_caps are available
+# We link them to OUR library, which then propagates to usermod
+target_link_libraries(mp_leddriver_lib INTERFACE 
+    idf::esp_lcd 
+    idf::driver
+    idf::esp_hw_support 
+    idf::esp_common
+)
+
 # Link to the main MicroPython target
 target_link_libraries(usermod INTERFACE mp_leddriver_lib)
-
-# Add dependency on ESP-IDF components
-# This ensures header paths for esp_lcd and driver are available
-target_link_libraries(usermod INTERFACE idf::esp_lcd idf::driver)
